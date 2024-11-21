849eb4448cc424e750ef0f5643dfa7d81cbd7f37
Rolande Dossou <rolandedossou76@gmail.com>
2024-11-21 17:10:22 +0100

    21-11-2024___17:10

--- database/Database.php
+++ database/Database.php
@@ -1,47 +1,56 @@
 <?php
+
 namespace DB;
+
+use Dotenv\Dotenv;
 use \PDO;
 use \PDOException;
 
-class Database {
-    public static ?PDO $instanceDb =null;
+class Database
+{
+    public static ?PDO $instanceDb = null;
 
-    //Configuration de la base de données 
-    private const DB_HOST  = "localhost";
-    private const DB_NAME = "todos_db";
-    private const DB_USER = "root";
 
-    private const DB_PASSWORD = "";
 
     /**
      * 
      */
 
-    private function __construct(){
+    private function __construct() {}
 
-    }
+    private function __clone() {}
 
-    private function __clone(){}
-    
-    public static function getInstance( ){
+    public static function getInstance()
+    {
+
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+        $dotenv->load();
+        //Configuration de la base de données 
+        $dbHost = $_ENV["DB_HOST"];
+        $dbName = $_ENV["DB_NAME"];
+        $dbUser = $_ENV["DB_USER"];
+        $dbPassword = $_ENV["DB_PASSWORD"];
+        $dbcharset = $_ENV["DB_CHARSET"];
+
         // s i l'instane est null, on la cré
         if (self::$instanceDb === null) {
             try {
-                self::$instanceDb = new PDO( "mysql:host=".
-                self::DB_HOST.";dbname=".self::DB_NAME.";charset=utf8mb4"
-                ,self::DB_USER,
-                 self::DB_PASSWORD,
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // lever des exceptions quand il y a des erreurs
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // renvoyer les data sous forme de tab associatif
-                ]);
+                self::$instanceDb = new PDO(
+                    "mysql:host=" .
+                        $dbHost . ";dbname=" . $dbName . ";charset=". $dbcharset ,
+                    $dbUser,
+                    $dbPassword,
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // lever des exceptions quand il y a des erreurs
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // renvoyer les data sous forme de tab associatif
+                    ]
+                );
             } catch (PDOException $e) {
-                exit("Echec de connexion à la bdd: ". $e-> getMessage());
+                exit("Echec de connexion à la bdd: " . $e->getMessage());
                 // dis("Echec de connexion à la bdd : " . $e ->getMessage ())
             }
         }
         // sinon, on la renvoie directement
         return self::$instanceDb;
     }
-
 }
